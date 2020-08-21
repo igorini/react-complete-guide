5f6e4b014b3851013045a9e041ba292324171784
Igor Maguskin <maguskin.igor@gmail.com>
2020-08-21 23:17:19 +0100

    Update nameChangedHandler

--- src/App.js
+++ src/App.js
@@ -12,14 +12,21 @@ class App extends Component {
     showPersons: false
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        {name: 'Alice', age: 18},
-        {name: event.target.value, age: 20},
-        {name: 'Charlie', age: 22}
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons})
   }
 
   deletePersonHandler = (personIndex) => {
@@ -53,7 +60,8 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
-              key={person.id}/>
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)}/>
           })}
         </div>
       );
