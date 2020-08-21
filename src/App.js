22a632b1baab02d345a283e84cb91a3efac51671
Igor Maguskin <maguskin.igor@gmail.com>
2020-08-21 21:23:42 +0100

    Pass a property to a switchNameHandler

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked')
     this.setState({
       persons: [
-        { name: 'Albert', age: 18},
+        { name: newName, age: 18},
         { name: 'Bob', age: 20},
         { name: 'Charlie', age: 23}
       ]
@@ -26,9 +26,14 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React App</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Football</Person>
+        <button onClick={this.switchNameHandler.bind(this, 'Albert')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Agatha')} > My Hobbies: Football</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
