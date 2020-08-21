41ac6b0186b5ced4f66fd0932c8b04941750fb6d
Igor Maguskin <maguskin.igor@gmail.com>
2020-08-21 20:27:24 +0100

    Add a state to App

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import './App.css';
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Alice', age: 18},
+      { name: 'Bob', age: 20},
+      { name: 'Charlie', age: 22}
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was clicked')
+    this.setState({
+      persons: [
+        { name: 'Albert', age: 18},
+        { name: 'Bob', age: 20},
+        { name: 'Charlie', age: 23}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>React App</h1>
-        <Person name="Alice" age="18" />
-        <Person name="Bob" age="20" >My Hobbies: Football</Person>
-        <Person name="Charlie" age="22" />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Football</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
