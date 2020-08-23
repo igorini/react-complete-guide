f023fe8f0fb8ffb7f0ffa89978f96319a5e26a3b
Igor Maguskin <maguskin.igor@gmail.com>
2020-08-23 16:39:28 +0100

    Convert Persons to a pure component

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import Person from "./Person/Person";
 
-const persons = props => {
-  console.log('[Persons.js] rendering...');
-  return props.persons.map((person, index) => (
-      <Person
-        click={() => props.clicked(index)}
-        name={person.name}
-        age={person.age}
-        key={person.id}
-        changed={(event) => props.changed(event, person.id)}/>
-    )
-  );
+class Persons extends PureComponent {
+
+  render() {
+    console.log('[Persons.js] rendering...');
+    return this.props.persons.map((person, index) => (
+        <Person
+          click={() => this.props.clicked(index)}
+          name={person.name}
+          age={person.age}
+          key={person.id}
+          changed={(event) => this.props.changed(event, person.id)}/>
+      )
+    );
+  }
 }
 
-export default persons;
+export default Persons;
