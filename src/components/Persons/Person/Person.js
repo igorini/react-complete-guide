a63604741a1acd964d3e818a579075e9259b2f27
Igor Maguskin <maguskin.igor@gmail.com>
2020-08-24 19:22:14 +0100

    Add a ref to a Person component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,13 +5,26 @@ import Auxi from "../../../hoc/Auxi";
 import withClass from "../../../hoc/withClass";
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.inputElementRef.current.focus();
+  }
+
   render() {
     console.log('[Person.js] rendering...');
     return (
       <Auxi>
         <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old.</p>
         <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name}/>
+        <input
+          ref={this.inputElementRef}
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}/>
       </Auxi>
     );
   }
